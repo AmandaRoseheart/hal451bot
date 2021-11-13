f47a35b623088ae83b4ae5e06475c6f04c31b9d2
Amanda Roseheart <amanda.roseheart@protonmail.com>
2021-11-13 11:43:27 +0100

    disable tasks for now

--- src/main/java/org/amandaroseheart/hal451bot/Main.java
+++ src/main/java/org/amandaroseheart/hal451bot/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         HAL451bot bot = new HAL451bot();
         bot.registerFeatures();
-        bot.registerTasks();
+//        bot.registerTasks();
         bot.start();
     }
 
